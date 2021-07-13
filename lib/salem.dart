a74f0073e625dd8908375abdba5408b4e4864d38
JONA T. FEUCHT <jona@keemail.me>
2021-07-13 16:28:51 +0200

    fix

--- lib/salem.dart
+++ lib/salem.dart
@@ -1,9 +1,4 @@
-library salem_engine;
-
-import 'package:dart_vlc/dart_vlc.dart';
-import 'package:desktop_window/desktop_window.dart';
-import 'package:flutter/material.dart';
-import 'package:universal_platform/universal_platform.dart';
+library engine;
 
 export 'core/persist/splash.dart';
 export 'package:salem/core/persist/route_observer.dart';
@@ -14,19 +9,7 @@ export 'package:salem/components/chat/models/core_chat.dart';
 export 'package:salem/components/global/main_menu.dart';
 export 'package:salem/components/visualnovel/gui/name_box.dart';
 export 'package:salem/components/visualnovel/models/core_speech.dart';
-
-abstract class Salem {
-  static Future<void> initialize() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    if (UniversalPlatform.isWindows ||
-        UniversalPlatform.isLinux ||
-        UniversalPlatform.isMacOS) {
-      await DesktopWindow.setMinWindowSize(Size(1000, 600));
-      await DesktopWindow.setMaxWindowSize(Size.infinite);
-    } else {}
-    if (UniversalPlatform.isWeb) {
-    } else {
-      DartVLC.initialize();
-    }
-  }
-}
+export 'package:salem/core/audio/global_audio.dart';
+export 'package:salem/components/visualnovel/models/bgm_volume.dart';
+export 'package:salem/components/visualnovel/models/voice_volume.dart';
+export 'package:salem/components/visualnovel/models/text_speed.dart';
