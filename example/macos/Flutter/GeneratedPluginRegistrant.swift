1097d39151bf8bccd618c1266278ca7df80de8df
JONA T. FEUCHT <jona@keemail.me>
2021-08-20 14:03:53 +0200

    bump

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,12 +6,14 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers
+import dart_vlc
 import path_provider_macos
 import salem
 import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
+  DartVlcPlugin.register(with: registry.registrar(forPlugin: "DartVlcPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SalemPlugin.register(with: registry.registrar(forPlugin: "SalemPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
